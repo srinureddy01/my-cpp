fdf2130e8e6db9d03f15bb2cecea34453e38b266
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2025-07-18 18:43:16 +0530

    Create chareters.cpp
    A
    AB
    ABC
    ABCD
    ABCDE

---
+++ pattersn/chareters.cpp
@@ -0,0 +1,18 @@
+ #include<iostream>
+ using namespace std;
+ void fun(int n)
+ {
+     for(int i=0 ; i < n ; i++)
+     {
+         for(char c = 'A' ; c <= 'A' + i ; c++)
+         {
+             cout << c;
+         }
+         cout << endl;
+     }
+ }
+ int main()
+ {
+     int n =5;
+     fun(n);
+ }
