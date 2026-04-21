52d51e210890e3d9ff86dea109fe53018727b01a
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2026-04-21 13:54:58 -0500

    Enhance vector operations in adding-remove.cpp
    Updated vector operations to demonstrate push_back, pop_back, emplace_back, insert, erase, and clear methods.

--- Data Structures/Sequence containers/adding-remove.cpp
+++ Data Structures/Sequence containers/adding-remove.cpp
@@ -1,24 +1,59 @@
 #include<iostream>
 #include<algorithm>
 #include<iterator>
-#include<vector>
 #include<numeric>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-    vector<int> val(3);
-    iota(val.begin() , val.end() , 21);
+    vector<int> val(7);
+    iota(val.begin(), val.end() ,7);
     
-    
-    //push_back()
-    val.push_back(12);
+    cout << "The original vector:";
     copy(val.begin() , val.end() , ostream_iterator<int>(cout, " "));
-    cout << endl;
     
-    //emplace_back
-    val.emplace_back(120);
-    copy(val.begin(),val.end(),ostream_iterator<int>(cout, " "));
+    //using the push_back()
+    cout << endl;
+    val.push_back(1);
+    cout << "pusing the ele 1 at the end: ";
+    copy(val.begin(), val.end(),ostream_iterator<int>(cout," "));
+    
+    //pop_back removes the last ele in the vector
+    cout << endl;
+    cout << "this is the pop operation and ";
+    val.pop_back();
+    cout << endl;
+    copy(val.begin() , val.end() , ostream_iterator<int>(cout, " "));
+    
+    //emplace add the ele at the back 
+    cout << endl;
+    cout << "emplac add the ele at the back" ;
+    val.emplace_back(123);
+    copy(val.begin() , val.end() , ostream_iterator<int>(cout, " "));
+    
+    //insert at the delet index 1 
+    cout << endl;
+    cout << "insert add the ele at the any index : " ;
+    val.insert(val.begin()+1 , 99);
+    copy(val.begin(), val.end() , ostream_iterator<int>(cout, " "));
+    
+    //erase at the index 1 delet the ele
+    cout << endl;
+    cout << "erase opeation remove the ele based on index val";
+    val.erase(val.begin()+1);
+    copy(val.begin() , val.end() , ostream_iterator<int>(cout, " "));
+    
+  // clear the entire vector
+    cout << endl;
+    cout << "Empt the vector operation here : ";
+    val.clear();
+    copy(val.begin(), val.end() , ostream_iterator<int>(cout , " "));
+    
+    
     
 }
+
+
+
