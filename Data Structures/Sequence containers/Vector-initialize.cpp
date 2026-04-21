f4b628d2c67e531cd079facb9147027dd9471172
GUDURU SRINIVASA REDDY <g.svreddyy@gmail.com>
2026-04-21 10:17:18 -0500

    Add examples of vector initialization in C++
    a sample methods to initialize the vectors

---
+++ Data Structures/Sequence containers/Vector-initialize.cpp
@@ -0,0 +1,14 @@
+// empty vector
+std::vector<int> a;
+
+// 5 elements all set to 0
+std::vector<int> b(5);
+
+//4 elements all set to 3
+std::vector<int> c(5, 3);
+
+//Initializer list 
+std::vector<int> d = {10,20,30,40};
+
+//copying of d
+std::vector<int> e(d);
